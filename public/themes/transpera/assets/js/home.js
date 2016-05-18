40c9db7139a619183df90f99d451b264604d917a
jimmy buyco <jimmy.buyco@republisys.com>
2016-05-18 15:00:23 +0800

    commit

--- public/themes/transpera/assets/js/home.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by jermaine.galman on 5/18/16.
- */
-$(document).ready(function(){
-    $(".button-collapse").sideNav();
-    $('#fonebayadFullPage').fullpage({
-        anchors: ['maine','about','career']
-    });
-});
