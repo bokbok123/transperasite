7fc6d5544e2cc1255f9afe19a187a6b88ec0f8cd
jermaine.galman <jermaine.galman@republisys.com>
2016-05-18 10:57:32 +0800

    splash

---
+++ app/views/section/splash.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jermaine.galman
+ * Date: 5/18/16
+ * Time: 10:57 AM
+ */ 
--- public/themes/transpera/partials/header.blade.php
+++ public/themes/transpera/partials/header.blade.php
@@ -3,7 +3,11 @@
         <a href="#" class="brand-logo">Logo</a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
             <li><a href="#about">About TransPera</a></li>
+            <li><a href="#features">Features</a></li>
             <li><a href="#career">Careers</a></li>
+            <li><a href="#partners">Partners</a></li>
+            <li><a href="#announcements">Announcements</a></li>
+            <li><a href="#contact">Contact Us</a></li>
         </ul>
     </div>
 </nav>
